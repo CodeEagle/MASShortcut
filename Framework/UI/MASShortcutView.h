a021f13f2d00358332f9887ef78a884f02351eda
CodeEagle <stasura@hotmail.com>
2025-04-25 10:18:40 +0800

    feat: 添加autoSet属性并优化代码格式

--- Framework/UI/MASShortcutView.h
+++ Framework/UI/MASShortcutView.h
@@ -2,24 +2,26 @@
 
 @class MASShortcut, MASShortcutValidator;
 
-extern NSString * _Nonnull const MASShortcutBinding;
+extern NSString *_Nonnull const MASShortcutBinding;
 
 typedef NS_ENUM(NSInteger, MASShortcutViewStyle) {
-    MASShortcutViewStyleDefault = 0,  // Height = 19 px
-    MASShortcutViewStyleTexturedRect, // Height = 25 px
-    MASShortcutViewStyleRounded,      // Height = 43 px
-    MASShortcutViewStyleFlat,
-    MASShortcutViewStyleRegularSquare
+  MASShortcutViewStyleDefault = 0,  // Height = 19 px
+  MASShortcutViewStyleTexturedRect, // Height = 25 px
+  MASShortcutViewStyleRounded,      // Height = 43 px
+  MASShortcutViewStyleFlat,
+  MASShortcutViewStyleRegularSquare
 };
 
 @interface MASShortcutView : NSView
 
-@property (nonatomic, strong, nullable) MASShortcut *shortcutValue;
-@property (nonatomic, strong, nullable) MASShortcutValidator *shortcutValidator;
-@property (nonatomic, getter = isRecording) BOOL recording;
-@property (nonatomic, getter = isEnabled) BOOL enabled;
-@property (nonatomic, copy, nullable) void (^shortcutValueChange)(MASShortcutView * _Nonnull sender);
-@property (nonatomic, assign) MASShortcutViewStyle style;
+@property(nonatomic, strong, nullable) MASShortcut *shortcutValue;
+@property(nonatomic, strong, nullable) MASShortcutValidator *shortcutValidator;
+@property(nonatomic, getter=isRecording) BOOL recording;
+@property(nonatomic, getter=isEnabled) BOOL enabled;
+@property(nonatomic) BOOL autoSet;
+@property(nonatomic, copy, nullable) void (^shortcutValueChange)
+    (MASShortcutView *_Nonnull sender);
+@property(nonatomic, assign) MASShortcutViewStyle style;
 
 /// Returns custom class for drawing control.
 + (nonnull Class)shortcutCellClass;
